c05d73e358f3bdffb12ff895fbe7d092b8befa91
jamesdimech808 <33218266+jamesdimech808@users.noreply.github.com>
2018-04-11 19:49:56 +0200

    Part 2
    Tried to set up the player paddle to respond to they up and down keys as they are pressed.
    
    I had a bit of trouble when i tried entering the code the first time, for some reason it was getting errors. However, i decided to delete the c# file and redo it and the code worked.
    
    The Players' paddle works.

--- Pong/Assets/Scripts/Player.cs
+++ Pong/Assets/Scripts/Player.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	//Variables to determine the Movement speed, Top and Bottom boundaries...
+	//and the Starting Position. 
+
+	public float moveSpeed = 8.0f;
+	public float topBounds = 8.3f;
+	public float bottomBounds = -8.3f;
+	public Vector2 startingPosition = new Vector2 (-13.0f, 0.0f);
+
+
 	// Use this for initialization
 	void Start () {
-		
+
+	transform.localPosition = (Vector3)startingPosition;		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Call the method
+		CheckUserInput ();
+
+	}
+
+	void CheckUserInput () {
 		
+		// check if the user is pressing Up or Down
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			if (transform.localPosition.y > topBounds) {
+				
+				//check to see if while we are moving up, 
+				//if we are passed the topBounds. 
+				transform.localPosition = new Vector3 (transform.localPosition.x, topBounds, transform.localPosition.z);
+			
+			} else {
+				//otherwise, we are going to allow the paddle to move
+				transform.localPosition += Vector3.up * moveSpeed * Time.deltaTime;
+
+			}
+
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+
+			//opposite for the bottom boundary
+			if (transform.localPosition.y < bottomBounds) {
+				
+				transform.localPosition = new Vector3 (transform.localPosition.x, bottomBounds, transform.localPosition.z);
+			
+			} else {
+
+				transform.localPosition += Vector3.down * moveSpeed * Time.deltaTime;
+
+			}
+		}
 	}
 }
