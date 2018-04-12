49f48ffe61163776dfdb1d2fc34f49e0f5327bd6
jamesdimech808 <33218266+jamesdimech808@users.noreply.github.com>
2018-04-12 20:15:05 +0200

    Part 7
    Changed the value for calculating the boundary from 2 to 1.8f and the ball managed to bounce off of the player paddle and pass through the ai paddle.
    
    Need to try and make it bounce back again with the ai paddle.

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -13,7 +13,7 @@ public class Ball : MonoBehaviour {
 	//Have the ball move left when initiated. 
 	public Vector2 ballDirection = Vector2.left;
 
-	//variables to use if powerups which will manipulate the gameobject's sizes for difficulty.
+	//variables to help set up the prefabs for collisions etc...
 	private float playerPaddleHeight, playerPaddleWidth, aiPaddleHeight, aiPaddleWidth, playerPaddleMaxX, playerPaddleMaxY, playerPaddleMinX, playerPaddleMinY, aiPaddleMaxX, aiPaddleMaxY, aiPaddleMinX, aiPaddleMinY, ballWidth, ballHeight;
 
 	private GameObject paddlePlayer, paddleAi;
@@ -34,13 +34,13 @@ public class Ball : MonoBehaviour {
 
 		//Part 6 - we are going to set the variables so that there is a max limit...
 		//to where the ball can move before it counts as colliding with the paddle. 
-		playerPaddleMaxX = paddlePlayer.transform.localPosition.x + playerPaddleWidth / 2;
-		playerPaddleMinX = paddlePlayer.transform.localPosition.x - playerPaddleWidth / 2;
+		playerPaddleMaxX = paddlePlayer.transform.localPosition.x + playerPaddleWidth / 1.8f;
+		playerPaddleMinX = paddlePlayer.transform.localPosition.x - playerPaddleWidth / 1.8f;
 
 		//this will check the X position of the paddle and create a boundary...
 		//for the collision. 
-		aiPaddleMaxX = paddleAi.transform.localPosition.x - aiPaddleWidth / 2;
-		aiPaddleMinX = paddleAi.transform.localPosition.x + aiPaddleWidth / 2;
+		aiPaddleMaxX = paddleAi.transform.localPosition.x - aiPaddleWidth / 1.8f;
+		aiPaddleMinX = paddleAi.transform.localPosition.x + aiPaddleWidth / 1.8f;
 		
 		//we will set our maxY and minY in the update method because the paddles...
 		//are constantly moving and the value is always changing. 
@@ -58,35 +58,34 @@ public class Ball : MonoBehaviour {
 	bool CheckCollision () {
 
 		//Part 6 - set our maxY and minY variables
-		//this will check the current Y position of our paddle...
-		//and add half the height of the paddle on both the top and bottom...
+		//this will check the current Y position of our paddle and add half the height of the paddle on both the top and bottom...
 		//to create the boundary for the paddle. 
-		playerPaddleMaxY = paddlePlayer.transform.localPosition.y + playerPaddleHeight / 2;
-		playerPaddleMinY = paddlePlayer.transform.localPosition.y - playerPaddleHeight / 2;
+		playerPaddleMaxY = paddlePlayer.transform.localPosition.y + playerPaddleHeight / 1.8f;
+		playerPaddleMinY = paddlePlayer.transform.localPosition.y - playerPaddleHeight / 1.8f;
 
-		aiPaddleMaxY = paddleAi.transform.localPosition.y + aiPaddleHeight / 2;
-		aiPaddleMinY = paddleAi.transform.localPosition.y - aiPaddleHeight / 2;
+		aiPaddleMaxY = paddleAi.transform.localPosition.y + aiPaddleHeight / 1.8f;
+		aiPaddleMinY = paddleAi.transform.localPosition.y - aiPaddleHeight / 1.8f;
 	
 		//This will check to see if the ball will collide with the paddles' edge. (an x collision (sides))
-		if (transform.localPosition.x - ballWidth / 2 < playerPaddleMaxX && transform.localPosition.x + ballWidth / 2 > playerPaddleMinX) {
+		if (transform.localPosition.x - ballWidth / 1.8f < playerPaddleMaxX && transform.localPosition.x + ballWidth / 1.8f > playerPaddleMinX) {
 			//this will check if the top/bottom of the paddles and ball will collide (a y collision)
-			if (transform.localPosition.y - ballHeight  / 2 < playerPaddleMaxY && transform.localPosition.y + ballHeight / 2 > playerPaddleMinY) {
+			if (transform.localPosition.y - ballHeight  / 1.8f < playerPaddleMaxY && transform.localPosition.y + ballHeight / 1.8f > playerPaddleMinY) {
 
 				//tell the ball to go right when it collides with the playerpaddle. 
 				ballDirection = Vector2.right;
 				//make sure the ball does not change direction from the inside of the paddle
-				transform.localPosition = new Vector3 (playerPaddleMaxX + ballWidth / 2, transform.localPosition.y, transform.localPosition.z);
+				transform.localPosition = new Vector3 (playerPaddleMaxX + ballWidth / 1.8f, transform.localPosition.y, transform.localPosition.z);
 				return true;
 
 			}
 		}
 
-		if (transform.localPosition.x + ballWidth /2 > aiPaddleMaxX && transform.localPosition.x - ballWidth /2 < aiPaddleMinX){
+		if (transform.localPosition.x + ballWidth /1.8f > aiPaddleMaxX && transform.localPosition.x - ballWidth /1.8f < aiPaddleMinX){
 
-			if (transform.localPosition.x - ballHeight / 2 < aiPaddleMaxY && transform.localPosition.y + ballHeight / 2 > aiPaddleMinY){
+			if (transform.localPosition.x - ballHeight / 1.8f < aiPaddleMaxY && transform.localPosition.y + ballHeight / 1.8f > aiPaddleMinY){
 
 				ballDirection = Vector2.left;
-				transform.localPosition = new Vector3 (aiPaddleMaxX - ballWidth /2, transform.localPosition.y, transform.localPosition.z);
+				transform.localPosition = new Vector3 (aiPaddleMaxX - ballWidth /1.8f, transform.localPosition.y, transform.localPosition.z);
 				return true;
 			}
 		}
